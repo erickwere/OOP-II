92203b74b6331184e0d2eb8430f4a67b88c113c5
James Erick <erick.were@strathmore.edu>
2016-04-22 09:41:10 +0300

    Added files via upload

---
+++ nested_loops.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+ using namespace std;
+
+ int main() {
+
+ for(int x = 0; x < 4; x = x + 1) {
+
+ for(int y = 0; y < 4; y = y + 1)
+
+ cout << y;
+
+ cout << "\n";
+
+ }
+
+ return 0;
+
+ }   //outputs four lines of 0123
